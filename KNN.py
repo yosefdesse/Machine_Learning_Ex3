427eefb3997f4881293140cb18a7b99d0a6bb74f
yosefdesse <yosefdassa@gmail.com>
2025-01-21 15:21:19 +0200

    update knn first try

--- KNN.py
+++ KNN.py
@@ -30,10 +30,10 @@ class KNN:
             idx = 0
             min = dist[0]
             for j in range(len(dist)):
-                if dist[j] < min and dist[j] != -1:
+                if dist[j] < min:
                     idx = j
                     min = dist[j]
-            labels.append(self.base_label(idx))
+            labels.append(self.base_label[idx])
             del dist[idx]  
         count0 = 0
         count1 = 0
@@ -42,18 +42,33 @@ class KNN:
                 count0 += 1
             else:
                 count1 += 1
+        
+        return 1 if count1>count0 else 0       
 
-        return max(count1, count0)         
-
-    def run_knn(self, test_data):
-        dist = []
-        idx = []
+    def predict_test(self, test_data):
         y_pred = []
         for i in range(len(test_data)):
+            dist = []
             for j in range(len(self.base)):
                 dist.append(self.p_distance(test_data[i], self.base[j], self.p))
             y_pred.append(self.predict_label(dist))
         return y_pred
+
+    def predict_train(self):
+        
+        y_pred = []
+        i = 0
+        while i != len(self.base):
+            indices = [j for j in range(len(self.base)) if j != i]
+            x_train = self.base[indices]
+            sample = self.base[i]
+            dist = []
+            for j in range(len(x_train)):
+                dist.append(self.p_distance(sample, x_train[j], self.p))
+            y_pred.append(self.predict_label(dist))
+            i+=1
+        return y_pred
+        
 
                 
 
--- main.py
+++ main.py
@@ -1,27 +1,52 @@
 import numpy as np 
-import random
 from KNN import KNN
+from sklearn.model_selection import train_test_split
 
-def accuracy(y_true, y_pred):
+def compute_error(y_true, y_pred):
     acc = np.sum(y_true == y_pred) / len(y_true)
-    return acc
+    return 1-acc
 
 def split_data(data1, data2):
-    combine_data = np.vstack(data1, data2)
-    labels = np.vstack([np.ones(len(data1)), np.zeros(len(data2))])
-    data = np.hstack([combine_data, labels])
-    indices = np.arange(len(data))
-    np.random.shuffle(indices)
-    train_indices = indices[:len(data) // 2]
-    test_indices = indices[len(data) // 2:]
-    return train_indices, test_indices
+    data = np.vstack([data1, data2])
+    labels = np.hstack([np.ones(len(data1)), np.zeros(len(data2))]) 
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        data, labels, test_size=0.5, stratify=labels, random_state=None
+    )
+
+    return x_train, x_test, y_train, y_test
+
+def run_knn(k, p, data1, data2):
+    empirical_errors = []
+    true_errors = []
+    knn = KNN(k, p)
+
+    for i in range(100):
+        train_data, test_data, train_label, test_label = split_data(data1, data2)
+        knn.init_base(train_data, train_label)
+        train_error = compute_error(knn.predict_train(), train_label)
+        test_error = compute_error(knn.predict_test(test_data), test_label)   
+        empirical_errors.append(train_error)
+        true_errors.append(test_error)
+    
+    avg_empirical_errors = np.mean(empirical_errors, axis=0)
+    avg_true_errors = np.mean(true_errors, axis=0)
+    diff = abs(avg_empirical_errors - avg_true_errors)
+
+    return avg_empirical_errors, avg_true_errors, diff
+
 
 def main (data_txt):
     versicolor, virginica = write_from_file(data_txt)
-    train_data, test_data = split_data(versicolor, virginica)
 
-    train_label = 
-
+    for i in range(0, 3):
+        for j in range(1, 10, 2):
+            avg_empirical_errors, avg_true_errors, diff = run_knn(i, j, versicolor, virginica)
+            if i == 0:
+                print(f'p : inf, k : {j}, Average empirical Error = {avg_empirical_errors},  Average true Error = {avg_true_errors}, Difference = {diff}')
+            else:
+                print(f'p : {i}, k : {j}, Average empirical Error = {avg_empirical_errors},  Average true Error = {avg_true_errors}, Difference = {diff}')
+        print('\n')
 
 
 def write_from_file(data_txt):
